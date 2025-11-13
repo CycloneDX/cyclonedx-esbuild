80c7f4ab861b0077f3ce349be1d8e8fb2986b003
Jan Kowalleck <jan.kowalleck@gmail.com>
2025-11-13 09:57:29 +0100

    docs: enrich examples
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- examples/simple/src/index.js
+++ examples/simple/src/index.js
@@ -1,5 +1,5 @@
 /*!
-This file is part of CycloneDX Webpack plugin.
+This file is part of CycloneDX generator for esbuild.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
